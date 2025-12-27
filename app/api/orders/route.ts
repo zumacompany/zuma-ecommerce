c0d447143b5b2c485cbf9572fe52c7db03a9029f
Thekng <41475597+Thekng@users.noreply.github.com>
2025-12-27 08:27:58 +0200

    New enviroments + Supabase + Typescript + Tailwind

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { supabaseAdmin } from '../../../../lib/supabase/server'
+import { supabaseAdmin } from '../../../lib/supabase/server'
 
 export async function POST(req: Request) {
   try {
